c8057d0e420f8e00a04529b2ba73dadf6642ad36
Kieran O'Neill <kieranroneill@googlemail.com>
2016-02-24 14:10:04 +0000

    Add Rindle content scripts

---
+++ src/scripts/content/rindle.js
@@ -0,0 +1,38 @@
+/*jslint indent: 2 */
+/*global $: false, document: false, togglbutton: false*/
+'use strict';
+
+togglbutton.render('.card__footer:not(.toggl)', { observe: true }, function (elem) {
+    var link;
+    var cardTitle = $('.toggl__card-title', elem);
+
+    var descFunc = function () {
+        return cardTitle.innerText;
+    };
+
+    link = togglbutton.createTimerLink({
+        className: 'rindle',
+        buttonType: 'minimal',
+        description: descFunc,
+        projectName: ''
+    });
+
+    $('.toggl__container', elem).appendChild(link);
+});
+
+togglbutton.render('.card__header:not(.toggl)', { observe: true }, function (elem) {
+    var link;
+    var cardTitle = $('.toggl__card-title', elem);
+
+    var descFunc = function () {
+        return cardTitle.innerText;
+    };
+
+    link = togglbutton.createTimerLink({
+        className: 'rindle',
+        description: descFunc,
+        projectName: ''
+    });
+
+    $('.toggl__overview-button').appendChild(link);
+});
